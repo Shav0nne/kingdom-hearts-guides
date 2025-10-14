bcfca53d5899753c259a81700e2ce3b5ce4f2027
Ishika <esoebhag@gmail.com>
2025-10-14 23:56:24 +0200

    initial commit

---
+++ resources/views/guide.blade.php
@@ -0,0 +1,3 @@
+<x-layout>
+    <h1>Hello From The Guide Page</h1>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,23 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view ('about');
+});
+
+Route::get('/contact', function () {
+    return view ('contact');
+});
+
+Route::get('/guide', function () {
+    return view ('guide');
+});
+
+Route::get('/login', function () {
+    return view ('auth.login');
 });
 
 Route::get('/dashboard', function () {
